3d029b7378815588478befca4dfde4ab1ea90e84
Your Name <you@example.com>
2023-06-20 15:21:37 -0700

    C functions

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,10 +9,18 @@
  */
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	 else if (n == 0)
-	}
+If (n >0)
+{
+_putchar(43);
+return (1);
+}
+else if (n < 0)
+{
+put-char(45);
+return (-1);
+} else
+{
+_putchar(48);
+return (0);
+}
 }
