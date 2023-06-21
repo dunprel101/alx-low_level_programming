58240ea070b919792ee16db0c082b403b5cbeadb
Your Name <you@example.com>
2023-06-21 03:35:32 -0700

    5 Signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,22 @@
 #include "main.h"
 
 /**
- * print_sign - Prints the sign of a number
- * @n: The number to be checked
- * Return: 1 for positive num, -1 for negative num or zero for anything else
+ * print_sign - checks for alphabetic character
+ * @n: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
  */
 int print_sign(int n)
 {
-      if (n > 0)
-       {
-         _putchar(43);
-         return (1);
-       }
-      else if (n < 0)
-       {
-         _putchar(45);
-         return (-1);
-       } 
-      else
-       {
-         _putchar(48);
-         return (0);
-       }
-      
+        if (n > 0)
+        {
+                _putchar('+');
+                return (1);
+        }
+        else if (n < 0)
+        {
+                _putchar('-');
+                return (-1);
+        }
+        _putchar('0');
+        return (0);
 }
