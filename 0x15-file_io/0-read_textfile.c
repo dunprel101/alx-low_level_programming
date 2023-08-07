10456ab5e930e7df85fe54e25e67104dc7de04d6
dunprel101 <dunkwuprecious77@gmail.com>
2023-08-07 15:24:48 -0700

    File input and output in C Programming

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /**
- * read_textfile- This file will print STDOUT.
- * @filename: text file being read
- * @letters: number of letters to be read
- * Return: w- the actual number of bytes read and printed
- *        0 when the function fails or the filename is NULL.
- */
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
